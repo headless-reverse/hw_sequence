ac39bfc6d5ed3fd385dab3e69e35bc5732273996
headless-reverse <scrcpyshell@gmail.com>
2026-03-04 05:52:44 +0100

    Update control_protocol.cpp

--- control_protocol.cpp
+++ control_protocol.cpp
@@ -1,1 +1,39 @@
+#include "control_protocol.h"
+#include <QtEndian>
+#include <cstring>
+#include <cstddef>
 
+
+uint8_t calculate_crc(const ControlPacket& pkt) {
+    const uint8_t* data_ptr = reinterpret_cast<const uint8_t*>(&pkt.magic);
+    size_t len = offsetof(ControlPacket, crc) - offsetof(ControlPacket, magic);
+    uint8_t crc = 0;
+    for (size_t i = 0; i < len; ++i) {crc ^= data_ptr[i];}
+    return crc;
+}
+
+
+ControlPacket createTouchPacket(ControlEventType type, uint16_t x, uint16_t y, uint16_t data) {
+    ControlPacket pkt;
+    std::memset(&pkt, 0, sizeof(ControlPacket));
+    pkt.head  = PROTOCOL_HEAD;
+    pkt.magic = qToBigEndian<uint32_t>(CONTROL_MAGIC);
+    pkt.type  = static_cast<uint8_t>(type);
+    pkt.x     = qToBigEndian<uint16_t>(x);
+    pkt.y     = qToBigEndian<uint16_t>(y);
+    pkt.data  = qToBigEndian<uint16_t>(data);
+    pkt.crc   = calculate_crc(pkt);
+    return pkt;
+}
+
+ControlPacket createKeyPacket(uint16_t keyCode) {return createTouchPacket(EVENT_TYPE_KEY, 0, 0, keyCode);}
+
+QByteArray packetToByteArray(const ControlPacket& packet) {
+    return QByteArray(reinterpret_cast<const char*>(&packet), sizeof(ControlPacket));}
+
+bool validatePacket(const ControlPacket& pkt) {
+    if (pkt.head != PROTOCOL_HEAD) return false;
+    if (pkt.magic != qToBigEndian<uint32_t>(CONTROL_MAGIC)) return false;
+    uint8_t expectedCrc = calculate_crc(pkt);
+    return (pkt.crc == expectedCrc);
+}
