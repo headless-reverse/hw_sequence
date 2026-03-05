2f2dd9e86642b4fe6d18d91d56020f3a3e9334b5
headless-reverse <scrcpyshell@gmail.com>
2026-03-05 04:22:30 +0100

    Update core_view.h

--- core_view.h
+++ core_view.h
@@ -6,34 +6,49 @@
 #include <QPushButton>
 #include <QProgressBar>
 #include <QLineEdit>
+#include <QSlider>
 #include <QVBoxLayout>
+#include <QPlainTextEdit>
 #include "core.h"
 
 class CoreView : public QDockWidget {
     Q_OBJECT
 public:
     explicit CoreView(Core *core, QWidget *parent = nullptr);
-
     void updateStatus(bool attached);
 
 private slots:
-    // Slot odbierający dane ze struktury shm_bridge_t
     void handleUpdate(uint32_t temp, uint64_t freq);
     void onAttachClicked();
     void onBoostClicked();
     void onSafeModeClicked();
+    void browseLuaScript();
+    void runLuaScript();
+    void stopLuaScript();
+    void appendLuaLog(const QString &line, bool isError);
 
 private:
+    Core *m_core = nullptr;
+    QLineEdit *m_ipEdit = nullptr;
+    QPushButton *m_btnConnect = nullptr;
+    QLabel *m_statusLed = nullptr;
+    QLabel *m_modelLabel = nullptr;
+    QProgressBar *m_tempBar = nullptr;
+    QLabel *m_freqValue = nullptr;
+    QLabel *m_batteryValue = nullptr;
+    QPushButton *m_boostBtn = nullptr;
+    QPushButton *m_safeBtn = nullptr;
+    QLabel *m_preciseVoltLabel = nullptr;
+    QSlider *m_preciseVoltSlider = nullptr;
+    QPushButton *m_applyVoltBtn = nullptr;
 
-    Core *m_core;
-    QLabel      *m_statusLed;
-    QLineEdit   *m_ipEdit;
-    QPushButton *m_btnConnect;
-    QProgressBar *m_tempBar;
-    QLabel       *m_freqValue;
-    QPushButton *m_boostBtn;
-    QPushButton *m_safeBtn;
-    QLabel *m_modelLabel;
+    QLineEdit *m_luaPathEdit = nullptr;
+    QLineEdit *m_luaArgsEdit = nullptr;
+    QLineEdit *m_luaInterpreterEdit = nullptr;
+    QPushButton *m_luaBrowseBtn = nullptr;
+    QPushButton *m_luaRunBtn = nullptr;
+    QPushButton *m_luaStopBtn = nullptr;
+    QPlainTextEdit *m_luaLog = nullptr;
 };
 
 #endif // CORE_VIEW_H
